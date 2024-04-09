429643526b14d910ca16b10e992f3284d275db70
Platipipus645 <76967479+Platipipus645@users.noreply.github.com>
2024-04-09 09:04:06 +0200

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -3,19 +3,19 @@ from leapyear import leapyear
 
 class TestLeapYear(unittest.TestCase):
 
-  def test_leap_year_divisible_by_400(self):
-    self.assertTrue(leapyear(2000))
+    def test_leap_year_divisible_by_400(self):
+        self.assertTrue(leapyear(2000))
+        
+    def test_leap_year_divisible_by_4_not_100(self):
+        self.assertTrue(leapyear(2004))  
     
-  def test_leap_year_divisible_by_4_not_100(self):
-    self.assertTrue(leapyear(2004))  
-
-  def test_not_leap_year_divisible_by_100_not_400(self):
-    self.assertFalse(leapyear(1900))  
-
-  def test_not_leap_year_not_divisible_by_4(self):
-    self.assertFalse(leapyear(2001))  
-
-  def test_negative_year(self):
-    self.assertFalse(leapyear(-2000)) 
+    def test_not_leap_year_divisible_by_100_not_400(self):
+        self.assertFalse(leapyear(1900))  
+    
+    def test_not_leap_year_not_divisible_by_4(self):
+        self.assertFalse(leapyear(2001))  
+    
+    def test_negative_year(self):
+        self.assertFalse(leapyear(-2000))
 
 
